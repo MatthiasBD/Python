c2ed241948a4b7fa0efc7b2b02a46f7ce4a03cea
MacUser <macuser@Mac-mini-van-MacUser-2.local>
2018-11-30 17:23:55 +0100

    homework les 8

---
+++ Les 8 Conditions & Loops/fizzbuzz.py
@@ -0,0 +1,14 @@
+number = int(raw_input("Select a number between 1 and 100: "))
+
+count = 1
+
+while count < (number + 1):
+    if (count % 15) == 0:
+        print "fizzbuzz"
+    elif (count % 5) == 0:
+        print "buzz"
+    elif (count % 3) == 0:
+        print "fizz"
+    else:
+        print str(count)
+    count += 1
